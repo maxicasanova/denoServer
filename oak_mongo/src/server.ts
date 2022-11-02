1e38f02be71fe887afb39089bb29374a7d4fab97
maxicasanova <maxi1602@gmail.com>
2022-11-01 21:50:16 -0300

    first deno server

---
+++ oak_mongo/src/server.ts
@@ -0,0 +1,13 @@
+import { Application, config } from "../depts.ts";
+
+import loggerMiddleware from "./middleware/logger.middleware.ts";
+import userRouter from "./routes/user.routes.ts";
+
+const app: Application = new Application();
+const { PORT } = await config();
+
+app.use(loggerMiddleware);
+app.use(userRouter.routes());
+
+app.listen({ port: Number(PORT) });
+console.log("Server listening http://127.0.0.1:3000");
